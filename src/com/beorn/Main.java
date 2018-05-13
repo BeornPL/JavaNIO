91ab645fc84f836880b1a20ef4107b79cd82cc53
BeornPL <37457935+BeornPL@users.noreply.github.com>
2018-05-13 11:56:01 +0200

    4th video Absolute and Relative Reads

--- src/com/beorn/Main.java
+++ src/com/beorn/Main.java
@@ -2,7 +2,6 @@ package com.beorn;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -13,53 +12,69 @@ public class Main {
         try (FileOutputStream binFile = new FileOutputStream("data.dat");
              FileChannel binChannel = binFile.getChannel()) {
 
+            ByteBuffer buffer = ByteBuffer.allocate(100);
             byte[] outputBytes = "Hello World!".getBytes();
-            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
-            int numBytes = binChannel.write(buffer);
-            System.out.println("numBytes written was: " + numBytes);
-
-            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-            intBuffer.putInt(245);
-            intBuffer.flip();
-            numBytes = binChannel.write(intBuffer);
-            System.out.println("numBytes written was: " + numBytes);
-
-            intBuffer.flip();
-            intBuffer.putInt(-98765);
-            intBuffer.flip();
-            numBytes = binChannel.write(intBuffer);
-            System.out.println("numBytes written was: " + numBytes);
-
-            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-            FileChannel channel = ra.getChannel();
-            outputBytes[0] = 'a';
-            outputBytes[1] = 'b';
+            buffer.put(outputBytes);
+            buffer.putInt(245);
+            buffer.putInt(-98765);
+            byte[] outputBytes2 = "Nice to meet you".getBytes();
+            buffer.put(outputBytes2);
+            buffer.putInt(1000);
             buffer.flip();
-            long numbBytesRead = channel.read(buffer);
-            if(buffer.hasArray()) {
-                System.out.println("byteBuffer = " + new String(buffer.array()));
-            }
+            binChannel.write(buffer);
 
-            //Absolute read
-            intBuffer.flip();
-            numbBytesRead = channel.read(intBuffer);
-            System.out.println(intBuffer.getInt(0));
-            intBuffer.flip();
-            numbBytesRead = channel.read(intBuffer);
-            System.out.println(intBuffer.getInt(0));
-
-            //Relative read
-            //            intBuffer.flip();
-            //            numbBytesRead = channel.read(intBuffer);
-            //            intBuffer.flip();
-            //            System.out.println(intBuffer.getInt());
-            //            intBuffer.flip();
-            //            numbBytesRead = channel.read(intBuffer);
-            //            intBuffer.flip();
-            //            System.out.println(intBuffer.getInt());
-
-            channel.close();
-            ra.close();
+//            ByteBuffer buffer = ByteBuffer.allocate(outputBytes.length);
+//            buffer.put(outputBytes);
+//            buffer.flip();
+//
+//            int numBytes = binChannel.write(buffer);
+//            System.out.println("numBytes written was: " + numBytes);
+//
+//            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+//            intBuffer.putInt(245);
+//            intBuffer.flip();
+//            numBytes = binChannel.write(intBuffer);
+//            System.out.println("numBytes written was: " + numBytes);
+//
+//            intBuffer.flip();
+//            intBuffer.putInt(-98765);
+//            intBuffer.flip();
+//            numBytes = binChannel.write(intBuffer);
+//            System.out.println("numBytes written was: " + numBytes);
+//
+//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+//            FileChannel channel = ra.getChannel();
+//            outputBytes[0] = 'a';
+//            outputBytes[1] = 'b';
+//            buffer.flip();
+//            long numbBytesRead = channel.read(buffer);
+//            if(buffer.hasArray()) {
+//                System.out.println("byteBuffer = " + new String(buffer.array()));
+////                System.out.println("byte buffer = " + new String(outputBytes));
+//            }
+//
+//            //Absolute read
+//            intBuffer.flip();
+//            numbBytesRead = channel.read(intBuffer);
+//            System.out.println(intBuffer.getInt(0));
+//            intBuffer.flip();
+//            numbBytesRead = channel.read(intBuffer);
+//            intBuffer.flip();
+//            System.out.println(intBuffer.getInt(0));
+//            System.out.println(intBuffer.getInt());
+//
+//            //Relative read
+//            //            intBuffer.flip();
+//            //            numbBytesRead = channel.read(intBuffer);
+//            //            intBuffer.flip();
+//            //            System.out.println(intBuffer.getInt());
+//            //            intBuffer.flip();
+//            //            numbBytesRead = channel.read(intBuffer);
+//            //            intBuffer.flip();
+//            //            System.out.println(intBuffer.getInt());
+//
+//            channel.close();
+//            ra.close();
 
 //            System.out.println("outputBytes = " + new String(outputBytes));
 
